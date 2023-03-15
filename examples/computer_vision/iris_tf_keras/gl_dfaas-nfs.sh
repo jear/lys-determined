ae9d89001d9d64ddbc978353c1d58f1fb84efdd3
jear <jear@users.noreply.github.com>
2023-03-15 18:09:30 +0100

    Update gl_dfaas-nfs.sh

--- examples/computer_vision/iris_tf_keras/gl_dfaas-nfs.sh
+++ examples/computer_vision/iris_tf_keras/gl_dfaas-nfs.sh
@@ -9,11 +9,6 @@ mkdir -p /mnt/pollen \
 && vault login --address=$VAULT_ADDRESS `cat /var/vault-token/vault-token-value` \
 && vault kv get --field=gl_id_rsa --address=$VAULT_ADDRESS pollen/iateam/gl-dfaas > /tmp/id_rsa_datafabric.pem \
 && chmod 0400 /tmp/id_rsa_datafabric.pem \
-&& vault kv get --field=EXTERNAL-IP --address=$VAULT_ADDRESS pollen/iateam/pollen-metrology-local > /usr/local/share/ca-certificates/EXTERNAL-IP.crt \
-&& vault kv get --field=EXTERNAL-IP-178-33-43-11 --address=$VAULT_ADDRESS pollen/iateam/pollen-metrology-local > /usr/local/share/ca-certificates/EXTERNAL-IP-178-33-43-11.crt \
-&& vault kv get --field=POLLEN-METROLOGY-CA --address=$VAULT_ADDRESS pollen/iateam/pollen-metrology-local > /usr/local/share/ca-certificates/POLLEN-METROLOGY-CA.crt \
-&& echo '178.33.43.11    srv-ad-01.pollen-metrology.local' >> /etc/hosts \
-&& echo '80.124.69.52    srv-ad-02.pollen-metrology.local' >> /etc/hosts \
 && ssh -i /tmp/id_rsa_datafabric.pem -o 'StrictHostKeyChecking no' -4 dfaasusr@16.103.6.95 -L 2049:localhost:2049 -N -f \
 && ssh -i /tmp/id_rsa_datafabric.pem -o 'StrictHostKeyChecking no' -4 dfaasusr@16.103.6.95 -L 111:localhost:111 -N -f \
 && mount -v -t nfs -o vers=3,port=2049,nolock,rw,proto=tcp,mountproto=tcp localhost:/fabrics/GL-DF-Beta/pollen /mnt/pollen \
