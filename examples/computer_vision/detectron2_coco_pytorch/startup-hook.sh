2e77790c08726633ff249b6d03f77ac61a513c23
Andrew Mendez <andrew.mendez@hpe.com>
2023-03-17 16:56:07 -0400

    resolved dependency issues to run on A100

--- examples/computer_vision/detectron2_coco_pytorch/startup-hook.sh
+++ examples/computer_vision/detectron2_coco_pytorch/startup-hook.sh
@@ -1,1 +1,5 @@
-pip install torch==1.10.0+cu102 torchvision==0.11.0+cu102 torchaudio==0.10.0 -f https://download.pytorch.org/whl/torch_stable.html
+conda install pytorch==1.10.1 torchvision==0.11.2 torchaudio==0.10.1 cudatoolkit=11.3 -c pytorch -c conda-forge
+git clone https://github.com/facebookresearch/detectron2 detectron2_repo 
+pip install -e detectron2_repo -f \
+  https://dl.fbaipublicfiles.com/detectron2/wheels/cu113/torch1.10/index.html
+pip install horovod==0.24.2
